5e828444addddd0f9c7a111cdc9acbf4e7835b8e
Jaaswini <147499075+Jaaswini@users.noreply.github.com>
2023-10-10 15:40:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Whitney: ["Whitney"],
+      GGSans: ["GGSans"],
+      Ginto: ["Ginto"],
+    },
+  },
+  plugins: [],
+};
